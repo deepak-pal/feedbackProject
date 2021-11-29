1297089915de32a79837fc7e48a18b998eb11922
deepak-pal <paldeepak306@gmail.com>
2021-11-29 22:56:35 +0530

    login stratgey

--- index.js
+++ index.js
@@ -1,22 +1,23 @@
 const express = require('express');
+const mongosse = require('mongoose');
+const cookieSession = require('cookie-session');
 const passport = require('passport');
-const GoogleStrategy = require('passport-google-oauth20').Strategy
+//const { MongoClient } = require('mongodb');
+const key = require('./config/key');
+require('./models/Users')
+require('./services//passport');
+mongosse.connect(key.mongoURI, { useNewUrlParser: true, useUnifiedTopology: true });
+
 const app = express();
-const keys = require('./config/key')
+app.use(
+    cookieSession({
+        maxAge: 30 * 24 * 60 * 60 * 1000,
+        keys: [key.cookieKey, key.cookieSecokey]
+    })
+);
 
-passport.use(new GoogleStrategy({
-    clientID: keys.googleClientID,
-    clientSecret: keys.googleClientSecretKey,
-    callbackURL: '/auth/google/callback'
-}, (accesToken, refreshToken, profile, done) => {
-    console.log("Access Token \\n Refresh Token \\n Profile", accesToken, refreshToken, profile);
-}))
-
-app.get('/auth/google/', passport.authenticate('google', {
-    scope: ['profile', 'email']
-}));
-
-app.get('/auth/google/callback', passport.authenticate('google'))
-
+app.use(passport.initialize());
+app.use(passport.session());
+require('./routes/authRoutes')(app);
 const PORT = process.env.PORT || 5000;
 app.listen(PORT);
